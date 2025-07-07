9d2bc6ee24fe79444e0fc8a5b5d4e56f846f48fb
FishTacoBidun <wolfie.essink@gmail.com>
2025-07-07 13:56:16 -0700

    Update essinkw_assignment2.c

--- essinkw_assignment2.c
+++ essinkw_assignment2.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <math.h>
 
 //constants
 
+//linked list
+struct Node
+{
+  //values
+  char title[100];
+  int year;
+  char languages[100];
+  double rating;
+  
+  Node* next
+}
 
 //function definitions
 
