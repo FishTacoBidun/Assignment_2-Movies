f0e9f8a51c3a078e3a949f9e6fbf829fc3cfcf01
FishTacoBidun <wolfie.essink@gmail.com>
2025-07-07 14:13:07 -0700

    Update essinkw_assignment2.c

--- essinkw_assignment2.c
+++ essinkw_assignment2.c
@@ -36,7 +36,65 @@ int main(void)
 //Uses code from movies.c example code
 int processMovieFile(char* filePath, Node** head)
 {
+  char* currLine = NULL;
+  size_t len = 0;
+  ssize_t read;
+  int count = 0;
+
+  FILE* movieFile = fopen(filePath, "r");
+  if (!movieFile) {
+      perror("Error opening file");
+      return 0;
+  }
+
+  // skip header line
+  getline(&currLine, &len, movieFile);
+
+  while ((read = getline(&currLine, &len, movieFile)) != -1) {
+        Node* newNode = malloc(sizeof(Node));
+        if (!newNode) {
+            perror("Memory allocation failed");
+            exit(EXIT_FAILURE);
+        }
+
+        //remove newline if present
+        if (currLine[strlen(currLine)-1] == '\n') {
+            currLine[strlen(currLine)-1] = '\0';
+        }
+
+        //parse line: title, year, languages, rating
+        char* token = strtok(currLine, ",");
+        strncpy(newNode->title, token, MAX);
+
+        token = strtok(NULL, ",");
+        newNode->year = atoi(token);
+
+        token = strtok(NULL, ",");
+        strncpy(newNode->languages, token, MAX);
+
+        token = strtok(NULL, ",");
+        newNode->rating = atof(token);
+
+        newNode->next = NULL;
+
+        //insert node at end of list
+        if (*head == NULL) {
+            *head = newNode;
+        } else {
+            Node* temp = *head;
+            while (temp->next != NULL)
+                temp = temp->next;
+            temp->next = newNode;
+        }
+
+        count++;
+    }
+
+    free(currLine);
+    fclose(movieFile);
+    printf("Processed file %s\n", filePath);
   
+    return count;
 }
 
 
